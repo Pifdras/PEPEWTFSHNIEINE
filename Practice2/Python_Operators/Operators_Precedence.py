d9546f8553d292ce296a0c46527e20e32c8c25e1
Yelzhas <4751kujo@gmail.com>
2026-02-03 14:54:26 +0500

    Delete Practice2/Python_Operators directory

--- Practice2/Python_Operators/Operators_Precedence.py
+++
@@ -1,8 +0,0 @@
-#Operator precedence describes the order in which operations are performed.
-
-#Parentheses has the highest precedence, meaning that expressions inside parentheses must be evaluated first:
-print((6 + 3) - (6 + 3))
-
-#Multiplication * has higher precedence than addition +, and therefore multiplications are evaluated before additions:
-
-print(100 + 5 * 3)
